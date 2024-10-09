2194513b2c24932be6300dfd1840b3bb288a7c46
ciroribba <ciro.ribba@gmail.com>
2024-10-09 16:52:13 -0300

    comunication btn - store

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -1,7 +1,25 @@
+'use client'
+import { useStore } from "@/src/store"
+
 export default function OrderSummary() {
+
+  const order = useStore(state => state.order)
+
   return (
     <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
       <h1 className="text-4xl text-center font-black">Mi pedido</h1>
+      {
+        order.length === 0 ? (
+          <p className="text-center my-10">No hay productos en el pedido</p>
+        ) : (
+          order.map((product) => (
+            <div key={product.id} className="flex justify-between mt-5">
+              <p>{product.name}</p>
+              <p>{product.quantity}</p>
+            </div>
+          ))
+        )
+      }
     </aside>
   )
 }
