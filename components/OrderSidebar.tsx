a89d44e1df739b3c3ebe8bb764d1acb4431f466e
ciroribba <ciro.ribba@gmail.com>
2024-10-09 10:03:43 -0300

    add images and conect to categories bkend

--- components/OrderSidebar.tsx
+++ components/OrderSidebar.tsx
@@ -1,4 +1,14 @@
-export default function OrderSidebar() {
+import { prisma } from '@/src/lib/prisma'
+
+async function getCategories() {
+  const categories = await prisma.category.findMany()
+  console.log('Las categorias son:', categories)
+}
+
+export default async function OrderSidebar() {
+  
+  await getCategories()
+
   return (
     <aside className="md:w-72 md:h-screen bg-white">
       OrderSidebar
